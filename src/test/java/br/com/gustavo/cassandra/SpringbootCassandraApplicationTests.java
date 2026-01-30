7bdc12caba53721af9d0c05820060a7bb8bc156e
Gustavo <gustavoirl456@outlook.com>
2026-01-29 23:51:50 -0300

    projeto criado

---
+++ src/test/java/br/com/gustavo/cassandra/SpringbootCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
