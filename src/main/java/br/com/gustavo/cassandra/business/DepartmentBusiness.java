9c85d67704592fdd5384211599d3737c970aab08
Gustavo <gustavoirl456@outlook.com>
2026-02-05 01:18:16 -0300

    buscando Departamento por id, tratando retorno do erro da api

--- src/main/java/br/com/gustavo/cassandra/business/DepartmentBusiness.java
+++ src/main/java/br/com/gustavo/cassandra/business/DepartmentBusiness.java
@@ -4,10 +4,12 @@ import br.com.gustavo.cassandra.dto.DepartmentDTO;
 import br.com.gustavo.cassandra.services.DepartmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("departments")
@@ -20,8 +22,14 @@ public class DepartmentBusiness {
     }
 
     @GetMapping
-    public ResponseEntity<List<DepartmentDTO>> findAll() {
-        List<DepartmentDTO> list = departmentService.findAll();
+    public ResponseEntity<List<DepartmentDTO>> buscarTodos() {
+        List<DepartmentDTO> list = departmentService.buscarTodos();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> buscarPorId(@PathVariable UUID id) {
+        DepartmentDTO obj = departmentService.buscarPorId(id);
+        return ResponseEntity.ok(obj);
     }
 }
--- src/main/java/br/com/gustavo/cassandra/services/DepartmentService.java
+++ src/main/java/br/com/gustavo/cassandra/services/DepartmentService.java
@@ -3,9 +3,12 @@ package br.com.gustavo.cassandra.services;
 import br.com.gustavo.cassandra.bean.Department;
 import br.com.gustavo.cassandra.dto.DepartmentDTO;
 import br.com.gustavo.cassandra.repositories.DepartmentRepository;
+import br.com.gustavo.cassandra.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,8 +20,17 @@ public class DepartmentService {
         this.departmentRepository = departmentRepository;
     }
 
-    public List<DepartmentDTO> findAll() {
+    public List<DepartmentDTO> buscarTodos() {
         List<Department> list = departmentRepository.findAll();
-        return list.stream().map(DepartmentDTO::new).collect(Collectors.toList());
+        return list
+                .stream()
+                .map(DepartmentDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public DepartmentDTO buscarPorId(UUID id) {
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+        return new DepartmentDTO(department);
     }
 }
